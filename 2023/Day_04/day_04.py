25e4dfeaf2f23aba0d05938bb02b71d91d7e44e7
SkyWa7ch3r <joel.g.w.dunstan@gmail.com>
2023-12-05 07:35:31 +0800

    Started numba cuda

--- 2023/Day_04/day_04.py
+++ 2023/Day_04/day_04.py
@@ -1,8 +1,15 @@
 import cudf as cd
 import cupy as cp
+from numba import cuda
 import time
 
-# TODO switch to the cards using a dictionary with numba, keep track of the number of points for solution one, number of copies for solution 2 and of course the card itself
+# TODO switch to the using numba
+@cuda.jit
+def get_solution(winning_nums, cards, copies):
+    points = 0
+    winning, card = cuda.grid(4)
+    if winning < winning_nums:
+        if card < ca
 
 def solution():
     input = cd.read_text("input.txt", delimiter="\n", strip_delimiters = True)
@@ -13,6 +20,10 @@ def solution():
     for idx in range(winning_nums.shape[0]):
         points[idx] = 2 ** (cp.sum(cp.in1d(winning_nums[idx], cards[idx])) - 1)
     print(cp.sum(points))
+    threadsperblock = 64
+    blockspergrid = (winning_nums.size + (threadsperblock - 1)) // threadsperblock
+    copies = cp.zeros_like(cards.shape[0])
+    get_solution[threadsperblock, blockspergrid](winning_nums, cards, copies)
 
 start = time.time()
 solution()
