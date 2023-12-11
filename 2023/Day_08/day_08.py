38a1e0df9e464dab82d60b0f4ffe7e505478b059
SkyWa7ch3r <joel.g.w.dunstan@gmail.com>
2023-12-11 10:06:45 +0800

    Day 8 done

--- 2023/Day_08/day_08.py
+++ 2023/Day_08/day_08.py
@@ -1,20 +1,68 @@
-import cupy as cp
 import cudf as cd
-import cugraph as cg
-import networkx as nx
+import itertools
 import time
+import numpy as np
+from numba import cuda
+
+@cuda.jit
+def get_all_z(node_list, end_list, left, right, instructions, steps):
+    node_idx = cuda.grid(1)
+    if node_idx < node_list.size:
+        found = False
+        instruction = 0
+        current = node_list[node_idx]
+        while not found:
+            if instructions[instruction] == 0:
+                current = left[current]
+            else:
+                current = right[current]
+            steps[node_idx] += 1
+            instruction += 1
+            if instruction == instructions.size:
+                instruction = 0
+            for num in end_list:
+                if num == current:
+                    found = True
+
 
 def solution():
-    input = cd.read_text('test1.txt', delimiter = '\n', strip_delimiters = True)
-    instructions = input.iloc[0]
+    input = cd.read_text('input.txt', delimiter = '\n', strip_delimiters = True)
+    instructions = input.iloc[0].strip()
     input = input[2:].str.partition('=').loc[:, [0,2]]
-    nodes = input[0]
-    edges = input[2].str.strip()
-    print(instructions, '\n', nodes, '\n', edges)
-
-    network = nx.DiGraph()
-    network.add_nodes_from(nodes.to_arrow().to_pylist())
-    print(network)
+    nodes = input[0].str.strip().to_arrow().to_pylist()
+    edges = input[2].str.strip().str.replace("\(|\)|\s", '').str.split(',')
+    edges = edges.to_arrow().to_pylist()
+    network = {}
+    current = 'AAA'
+    end = 'ZZZ'
+    network = {node: {'L' : edge[0], 'R': edge[1]} for node, edge in zip(nodes, edges)}
+    # Solution for Part 1
+    if 'AAA' in list(network.keys()):
+        steps = 0
+        for instruction in itertools.cycle(instructions):
+            current = network[current][instruction]
+            steps += 1
+            if current == end:
+                break
+        print(f"The number of steps to ZZZ was: {steps}")
+    # Part 2 Solutions
+    # Convert all to numbers for parallel compute using GPU
+    node_dict = {node: idx for idx, node in enumerate(nodes)}
+    left = []
+    right = []
+    for edge in edges:
+        left.append(node_dict[edge[0]])
+        right.append(node_dict[edge[1]])
+    left = np.array(left)
+    right = np.array(right)
+    numbered_instructions = np.array(list(instructions.replace('L', '0').replace('R', '1')), dtype = np.int64)
+    node_list = np.array([node_dict[a_node] for a_node in filter(lambda x: x[-1] == 'A' ,list(network.keys()))])
+    end_list = np.array([node_dict[z_node] for z_node in filter(lambda x: x[-1] == 'Z' ,list(network.keys()))])
+    steps = np.zeros_like(node_list)
+    threads = 1024
+    blocks = (node_list.size + threads - 1) // threads
+    get_all_z[blocks, threads](node_list, end_list, left, right, numbered_instructions, steps)
+    print(f"The number of steps required to get all z {np.lcm.reduce(steps)}")
 
 
 start = time.time()
